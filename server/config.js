8176cf6889976207d260bb7b813efa5324575bae
Yaniv Amiri <yanivam123@gmail.com>
2019-12-05 13:44:45 -0500

    Added my own database

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@
 export const {
   PORT = 4000,
   NODE_ENV = 'development',
-  MONGO_URI = 'mongodb+srv://sairat:sairat@cluster0-06fga.mongodb.net/testone?retryWrites=true&w=majority',
+  MONGO_URI = 'mongodb+srv://sairat:sairat@cluster0-06fga.mongodb.net/yanivtest?retryWrites=true&w=majority',
   SESS_NAME = 'sid',
   SESS_SECRET = 'secret!session',
   SESS_LIFETIME = 1000 * 60 * 60 * 2
